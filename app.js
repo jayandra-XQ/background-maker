ee83bfbbc720885436b52ece83975857676a5b80
Jayandra Hamal <salonhamal278@gmail.com>
2025-01-23 17:29:14 +0545

    conect to mongoDB database

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 // Import require modules
+import 'dotenv/config'
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url'
+
+import { connectDB } from './lib/db.js';
+import cookieParser from 'cookie-parser';
 
 
 // Get the current directory path
@@ -11,14 +15,16 @@ const __dirname = path.dirname(__filename);
 //initialize the app
 const app = express();
 
+app.use(express.json())
+app.use(cookieParser());
+
 //set the view engine to ejs
 app.set('view engine', 'ejs');
 
 // Serve static files (CSS, JS, images, etc.)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// set port to 4000
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 // Define routes
 app.get('/', (req, res) => {
@@ -32,4 +38,5 @@ app.get('/', (req, res) => {
 // Start the server on port 4000
 app.listen(PORT, () => {
   console.log(`server is listening on ${PORT}`);
+  connectDB(); // Connect to MongoDB
 });

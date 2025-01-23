6aeba177bc9d5893ae7344c3096978524695bee0
Jayandra Hamal <salonhamal278@gmail.com>
2025-01-23 18:22:05 +0545

    authentication routes

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ import 'dotenv/config'
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url'
+import session from 'express-session';
+import bodyParser from 'body-parser';
+
+import authRoutes from './routes/auth.js'
 
 import { connectDB } from './lib/db.js';
-import cookieParser from 'cookie-parser';
 
 
 // Get the current directory path
@@ -14,12 +17,22 @@ const __dirname = path.dirname(__filename);
 
 //initialize the app
 const app = express();
-
 app.use(express.json())
-app.use(cookieParser());
+
 
 //set the view engine to ejs
 app.set('view engine', 'ejs');
+
+// express session
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false },
+}));
+
+app.use('/', authRoutes);
 
 // Serve static files (CSS, JS, images, etc.)
 app.use(express.static(path.join(__dirname, 'public')));
@@ -34,6 +47,14 @@ app.get('/', (req, res) => {
   }); // Render 'index.ejs' template
 });
 
+
+app.get('/protected', (req, res) => {
+  if (req.session.user) {
+    res.send(`Welcome, ${req.session.user.username}! This is protected content.`);
+  } else {
+    res.redirect('/login');
+  }
+});
 
 // Start the server on port 4000
 app.listen(PORT, () => {
---
+++ routes/auth.js
@@ -0,0 +1,65 @@
+import express from 'express';
+import User from '../models/user.js'
+
+
+
+const router = express.Router();
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+
+// route for register 
+router.post('/register', async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ name })
+    if (existingUser) {
+      return res.send('Username already exist')
+    }
+
+    const newUser = new User({ name, email, password })
+    await newUser.save()
+    res.redirect('/login')
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error registering user.');
+  }
+})
+
+router.get('/login', (req, res) => {
+  res.render('login')
+});
+
+// route for login
+router.post('/login', async (req, res) => {
+  const { name, password } = req.body;
+
+  try {
+    const user = await User.findOne({ name, password })
+
+    if (user) {
+      req.session.user = user
+      res.redirect('/protected')
+    } else {
+      res.send('Invalid credentials')
+    }
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Error logging in.')
+  }
+})
+
+// route for logout
+router.get('/logout', (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/login')
+  })
+})
+
+export default router;
+
+
